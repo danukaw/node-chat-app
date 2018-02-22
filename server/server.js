2b8f001f796cb3decad5da9ffeed2c11b0c68191
danukaw <danuka.wijetunge@ebuilder.com>
2018-02-22 17:56:30 +0530

    node chat app initiating..

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
+const path = require('path');
 const express = require('express');
 let app = express();
+let publicPath = path.join(__dirname, '../public');
 
 let PORT = process.PORT || 3000
 
-app.use(express.static('../public'));
+app.use(express.static(publicPath));
+
+app.use((req,res, next)=> {
+    if (req.headers['x-forwarded-proto'] === 'https') {
+        
+        res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
+    }
+});
 
 app.listen(PORT, ()=> {
     console.log(`server has start at port ${PORT}`)
